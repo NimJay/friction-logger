5a059d2dcadcbc9d857738a516fcff32813c0dc1
Nim Jayawardena <nimjay@google.com>
2021-04-23 10:20:42 -0400

    Remove preact-cli-generated "profile" route

--- front-end/src/components/app.js
+++ front-end/src/components/app.js
@@ -1,18 +1,13 @@
 import { h } from 'preact';
 import { Router } from 'preact-router';
 
-import Header from './header';
-
 // Code-splitting is automated for `routes` directory
 import HomeRoute from '../routes/home-route';
-import Profile from '../routes/profile';
 
 const App = () => (
   <div id="app">
     <Router>
       <HomeRoute path="/" />
-      <Profile path="/profile/" user="me" />
-      <Profile path="/profile/:user" />
     </Router>
   </div>
 )
