9cd8ddf152ed838dd08e9d65817c9799de352468
Nim Jayawardena <nimjay@google.com>
2021-04-26 11:04:42 -0400

    Implement <FrictionLogsRoute>

---
+++ front-end/src/components/loading-section/index.js
@@ -0,0 +1,10 @@
+import { h } from 'preact';
+import style from './style.css';
+
+const LoadingSection = () => (
+  <section className={style.LoadingSection}>
+    Loading...
+  </section>
+);
+
+export default LoadingSection;
--- front-end/src/routes/friction-logs-route/index.js
+++ front-end/src/routes/friction-logs-route/index.js
@@ -1,5 +1,8 @@
 import { Component, h } from 'preact';
 import { route } from 'preact-router';
+import { Link } from 'preact-router/match';
+import { getMyFrictionLogs, createFrictionLog } from '../../back-end-api';
+import LoadingSection from '../../components/loading-section';
 import { isSignedInViaGoogle } from '../../google-sign-in';
 import style from './style.css';
 
@@ -8,20 +11,65 @@ class FrictionLogsRoute extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      isInitialDataLoaded: false,
+      isCreatingFrictionLog: false,
+      frictionLogs: [],
     };
+    this.createFrictionLog = this.createFrictionLog.bind(this);
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     if (!isSignedInViaGoogle()) {
       route('/');
     }
+    await this.loadFrictionLogs();
+    this.setState({ isInitialDataLoaded: true });
+  }
+
+  async loadFrictionLogs() {
+    const { frictionLogs } = await getMyFrictionLogs();
+    this.setState({ frictionLogs });
+  }
+
+  createFrictionLog() {
+    this.setState({ isCreatingFrictionLog: true }, async () => {
+      const { frictionLog } = await createFrictionLog("Untitled Friction Log");
+      route(`/friction-logs/${frictionLog.id}`);
+    });
   }
 
   render() {
+    const {
+      isInitialDataLoaded, frictionLogs, isCreatingFrictionLog
+    } = this.state;
+
+    if (!isInitialDataLoaded) {
+      return (
+        <div className={style.FrictionLogsRoute}>
+          <LoadingSection />
+        </div>
+      );
+    }
+
+    const frictionLogLis = frictionLogs.map((frictionLog) => (
+      <li key={frictionLog.id}>
+        <Link href={`/friction-logs/${frictionLog.id}`}>
+          {frictionLog.name}
+        </Link>
+      </li>
+    ));
+
     return (
-      <div>
-        <main class={style.FrictionLogsRoute}>
-          FrictionLogsRoute
+      <div className={style.FrictionLogsRoute}>
+        <main>
+          <h1>My Friction Logs</h1>
+          {frictionLogs.length === 0
+            && <p>You don't have any friction logs.</p>}
+          {frictionLogs.length > 0
+            && <ol>{frictionLogLis}</ol>}
+          <button onClick={this.createFrictionLog} disabled={isCreatingFrictionLog}>
+            + New Friction Log
+          </button>
         </main>
       </div>
     );
