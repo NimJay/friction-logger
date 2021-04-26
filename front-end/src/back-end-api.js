b50ed8652997a44302a177e476587bc8f9c84752
Nim Jayawardena <nimjay@google.com>
2021-04-26 11:00:27 -0400

    Add back-end-api.js
    All requests to the server from the front-end should
    go through functions inside back-end-api.js.

---
+++ front-end/src/back-end-api.js
@@ -0,0 +1,30 @@
+import { getGoogleIdToken } from './google-sign-in';
+
+const SERVER_URL = 'http://localhost:8080';
+
+function getMyFrictionLogs() {
+  return makePostRequest('/back-end/get-my-friction-logs', {
+    googleIdToken: getGoogleIdToken(),
+  });
+}
+
+function createFrictionLog(frictionLogName) {
+  return makePostRequest('/back-end/create-friction-log', {
+    name: frictionLogName,
+    googleIdToken: getGoogleIdToken(),
+  });
+}
+
+function makePostRequest(path, body) {
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+  const promise = fetch(SERVER_URL + path, {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers,
+  });
+  return promise.then((response) => response.json());
+}
+
+export { getMyFrictionLogs, createFrictionLog };
